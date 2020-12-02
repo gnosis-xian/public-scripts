02626aa0a71bc086b003b8b81d19751a9ba56e80
Gnosis <gaojing1996@vip.qq.com>
2020-12-02 18:00:48 +0800

    opt startup shell script.

--- luck_number/start.sh
+++ luck_number/start.sh
@@ -1,12 +1,13 @@
-cd /predict_Lottery_ticket && git pull && python predict_api.py
-#
-#cd / && \
-#apt install git -y && \
-#git clone https://github.com/gnosis-xian/predict_Lottery_ticket.git && \
-#cd predict_Lottery_ticket && \
-#mkdir -p data && \
-#pip install -r requirements.txt -i https://pypi.douban.com/simple/ && \
-#pip install pandas lxml -i https://pypi.douban.com/simple/ && \
-#python get_train_data.py && \
-#python train_model.py && \
-#python predict_api.py
+if cd /predict_Lottery_ticket ; then
+  git pull && python predict_api.py
+else
+  cd / && \
+  apt install git -y && \
+  git clone https://github.com/gnosis-xian/predict_Lottery_ticket.git && \
+  cd predict_Lottery_ticket && \
+  mkdir -p data && \
+  pip install -r requirements.txt -i https://pypi.douban.com/simple/ && \
+  pip install pandas lxml -i https://pypi.douban.com/simple/ && \
+  python get_train_data.py && \
+  python train_model.py && \
+  python predict_api.py
