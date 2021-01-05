b316bc35e63c2449eb9262575b5dca13bfd1f3e4
Gnosis <gaojing1996@vip.qq.com>
2021-01-05 17:05:19 +0800

    adding pull.sh.

---
+++ ssq/pull.sh
@@ -0,0 +1,8 @@
+cd /root/ssq && git pull
+cd /root/public-scripts && git pull
+
+cd /root/ssq && python3 ssq_data.py
+cd /root/ssq && python3 ssq.py
+cd /root/ssq && python3 ssq_test.py
+cd /root/ssq && python3 ssq4all.py
+cd /root/ssq && python3 ssq4all_test.py
