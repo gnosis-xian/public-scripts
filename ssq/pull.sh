e30c8b7edd86a7f2c7b45d3a9879f8de23fabe43
Gnosis <gaojing1996@vip.qq.com>
2021-01-20 10:33:02 +0800

    adding pull.sh.

--- ssq/pull.sh
+++ ssq/pull.sh
@@ -9,10 +9,15 @@ echo "数据导入完成"
 
 echo "ssq.py ssq_test.py 开始执行..."
 #cd /root/ssq && python3 ssq.py
-cd /root/ssq && python3 ssq_test.py
+#cd /root/ssq && python3 ssq_test.py
 echo "ssq.py ssq_test.py 执行完成"
 
 echo "ssq4all.py ssq4all_test.py 开始执行..."
 #cd /root/ssq && python3 ssq4all.py
-cd /root/ssq && python3 ssq4all_test.py
+#cd /root/ssq && python3 ssq4all_test.py
 echo "ssq4all.py ssq4all_test.py 执行完成"
+
+echo "dlt4all.py dlt4all_test.py 开始执行..."
+cd /root/ssq && python3 dlt4all.py
+cd /root/ssq && python3 dlt4all_test.py
+echo "dlt4all.py dlt4all_test.py 执行完成"
